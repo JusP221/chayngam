5ad240a917f2d1231cbf134f00b15a19fb913b52
JusP221 <byakushikis@gmail.com>
2021-12-08 11:32:26 +0700

    first commit

---
+++ app/src/main/java/com/example/foregroundservice/ExampleService.java
@@ -0,0 +1,57 @@
+package com.example.foregroundservice;
+
+import static com.example.foregroundservice.App.CHANNEL_ID;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class ExampleService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //String input = intent.getStringExtra("inputExtra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        //broadcastIntent.putExtra("toastMessage", input);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this,
+                0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Example Service")
+                .setContentText("123")
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(pendingIntent)
+                .addAction(R.mipmap.ic_launcher, "Scan", actionIntent)
+                .build();
+
+        startForeground(1, notification);
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
